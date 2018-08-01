fade340f29f27e2e9005ac05f6c671679da56aa6
kammerla <heinz.kammerlander@gmail.com>
2018-08-01 16:41:20 +0200

    version 1

--- routing/src/routing/Routing.java
+++ routing/src/routing/Routing.java
@@ -4,7 +4,13 @@ public class Routing {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		System.out.println("Routing Start");
+		
+		TestDijkstraAlgorithm test = new TestDijkstraAlgorithm();
+		test.testExcute();
+		
+		System.out.println("Routing End");
 	}
 
 }
